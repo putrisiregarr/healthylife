e70bc122547e9a34d8053fd1f12d656dbfe7a9b7
putrisiregarr <siregarrputri20@gmail.com>
2021-10-29 22:14:45 +0700

    healthylife

---
+++ app/src/main/java/com/example/healthylife_android6/log_breakfast.java
@@ -0,0 +1,14 @@
+package com.example.healthylife_android6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class log_breakfast extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_breakfast);
+    }
+}
---
+++ app/src/main/java/com/example/healthylife_android6/log_dinner.java
@@ -0,0 +1,4 @@
+package com.example.healthylife_android6;
+
+public class log_dinner {
+}
