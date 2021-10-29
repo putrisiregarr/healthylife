a87d247f2bd779e9126b149b97fa6bfa1f499721
RafikaFadilaDaulay <rafikafadila15@gmail.com>
2021-10-30 00:53:35 +0700

    food calori, add food, reminder water, progres

---
+++ app/src/main/java/com/example/healthylife_android6/MainActivity2.java
@@ -0,0 +1,12 @@
+package com.example.healthylife_android6;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
